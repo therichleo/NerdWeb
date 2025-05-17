843861f30f2e926bf2b93febe003ca32ee4f7e2b
TheRichLeo.dev <leonardo.valdes@mail.udp.cl>
2025-05-17 06:52:18 +0200

    nuevo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import { PORT } from './config.js'
+import { UserRepository } from './user-repository.js'
 
 const app = express()
+app.use(express.json()) //express.json ayuda a mirar req.body 
 
 
 app.get('/', (req, res) => {
@@ -9,9 +11,20 @@ app.get('/', (req, res) => {
 })
 
 app.post('/login', (req,res) => {})
-app.post('/register', (req,res) => {})
+app.post('/register', (req,res) => {
+    const { username, password, email, anonimato } = req.body
+    console.log(req.body)
+
+    try{ 
+        const id = UserRepository.create({ username, password, email, anonimato })
+        res.send({id})
+    } catch(error){
+        //Lo mejor no es mandar el error entero, si no aclarar mejor (proximamente)
+        res.status(400).send(error.message)
+    }
+})
 app.post('/logout', (req,res) => {})
-app.get('/protected', (reeq,res) => {})
+app.get('/protected', (req,res) => {})
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
--- src/user-repository.js
+++ src/user-repository.js
@@ -5,22 +5,40 @@ const User = Schema('User', {
     id: { type: String, required: true },
     username: { type: String, required: true },
     password: {type: String, required: true},
-    name : { type : String, required: true },
+    email: {type: String, required: true},
     anonimato : { type: Boolean, required: true },
 })
 
 export class UserRepository {
     static create ({ username, password, email, anonimato }) {
-        //1. validaciones (password with simbols and signals... coming soon)
+        //1. Verificar email
+        //if(typeof email != 'string') throw new Error('Password must be a string')
+        //const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        //if (!emailRegex.test(email)) throw new Error('Email format is invalid');
+        //const emailExists = User.findOne({ email });
+        //if (emailExists) throw new Error('Email already registered');
+        //2. validaciones password 
         if(typeof username != 'string') throw new Error('username must be a string')
         if(username.length < 3) throw new Error('Username must be at least 3 characters lenght')
         if(typeof password != 'string') throw new Error('Password must be a string')
-        if(password.length < 6) throw new Error('Password must be at least 6 characters lenght')
-        if(!password.includes("$") || !password.includes("#") || !password.includes("&")) throw new Error('Password must have a character like "#,$,&"')
+        //if(password.length < 6) throw new Error('Password must be at least 6 characters lenght')
+        //if(!password.includes("$") || !password.includes("#") || !password.includes("&")) throw new Error('Password must have a character like "#,$,&"')
+        //3. Asegurarse que el username no existe
+        const user = User.findOne({ username })
+        if(user) throw new Error('Username already exists, change your username')
+        //4. Creacion de ID random (Mejor opcion base de datos con ID unico)
+        const id = crypto.randomUUID()
+        //5. Anonimato Booleano
+        if(typeof anonimato != 'boolean') throw new Error('anonimato must be a boolean (true or false)')
 
-
-
-        //2. Asegurarse que el username no existe
+        User.create({
+            id,
+            username,
+            password,
+            email,
+            anonimato
+        }).save()
+        return id
     }
     static login ({ username, password }) {}
 }
