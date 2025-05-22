0a3f36f26bb5c30ae990cdd9f61ac0d54181d8d6
therichleo <leonardo.valdes@mail.udp.cl>
2025-05-22 16:08:00 -0400

    Avance con nodemon live server para fronted

--- src/index.js
+++ src/index.js
@@ -1,65 +1,89 @@
-import express, { json } from 'express'
-import jwt from 'jsonwebtoken'
-import { PORT, LLAVE_SECRETA } from './config.js'
-import { UserRepository } from './user-repository.js'
-import { engine } from 'express-handlebars'
+import express, { json } from 'express';
+import jwt from 'jsonwebtoken';
+import { PORT, LLAVE_SECRETA } from './config.js';
+import { UserRepository } from './user-repository.js';
+import { engine } from 'express-handlebars';
+import livereload from 'livereload';
+import connectLivereload from 'connect-livereload';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
-const app = express()
-app.use(express.json()) //express.json ayuda a mirar req.body 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// Configure handlebars
+// Configuracion para hacer un tipo de live server, esto ayuda a mirar las carpetas
+const liveReloadServer = livereload.createServer({
+  exts: ['js', 'html', 'css', 'handlebars'],
+  debug: true,
+});
+
+liveReloadServer.watch([
+  dirname(__dirname) + '/views',
+  dirname(__dirname) + '/public',
+  dirname(__dirname) + '/src',
+]);
+
+const app = express();
+
+app.use(connectLivereload());
+app.use(express.json()); //express.json ayuda a mirar req.body
+
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('home', {username: 'therichleo'} )
-})
+  res.render('home', { username: 'therichleo' });
+});
 
-app.post('/login', async (req,res) => {
-    const { email, password } = req.body
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
 
-    try{
-        const user = await UserRepository.login({ email, password })
-        const token = jwt.sign({ id: user.id }, LLAVE_SECRETA, {
-            expiresIn: '1h'
-        })
-        res.send({ user })
-    } catch(error){
-        res.status(401).send(error.message)
-    }
-})
+  try {
+    const user = await UserRepository.login({ email, password });
+    const token = jwt.sign({ id: user.id }, LLAVE_SECRETA, {
+      expiresIn: '1h',
+    });
+    res.send({ user });
+  } catch (error) {
+    res.status(401).send(error.message);
+  }
+});
 
+app.post('/register', async (req, res) => {
+  const { username, password, email, anonimato } = req.body;
+  console.log(req.body);
 
-app.post('/register', async (req,res) => {
-    const { username, password, email, anonimato } = req.body
-    console.log(req.body)
+  try {
+    const id = await UserRepository.create({
+      username,
+      password,
+      email,
+      anonimato,
+    });
+    res.send({ id });
+  } catch (error) {
+    //Lo mejor no es mandar el error entero, si no aclarar mejor (proximamente)
+    res.status(400).send(error.message);
+  }
+});
 
-    try{ 
-        const id = await UserRepository.create({ username, password, email, anonimato })
-        res.send({id})
-    } catch(error){
-        //Lo mejor no es mandar el error entero, si no aclarar mejor (proximamente)
-        res.status(400).send(error.message)
-    }
-})
-
-app.post('/logout', (req,res) => {})
-app.get('/profile', (req,res) => {
-
-})
+app.post('/logout', (req, res) => {});
+app.get('/profile', (req, res) => {});
 
 app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`)
-})
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
 
+// refresca automaticamente cuando se hace un control + S
+liveReloadServer.server.once('connection', () => {
+  setTimeout(() => {
+    liveReloadServer.refresh('/');
+  }, 100);
+});
 
-
-
-
-app.use((req,  res) => {
-    res.status(404).send('<h1>404</>')
-})
-
+app.use((req, res) => {
+  res.status(404).send('<h1>404</>');
+});
